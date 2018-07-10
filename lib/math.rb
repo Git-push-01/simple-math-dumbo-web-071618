1a4e3567de18e7d8116606df65a9700d2fe6d6c0
Hugo Escandon <hescandon6@gmail.com>
2018-07-10 13:59:31 +0000

    automatically backed up by learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,16 +5,24 @@ end
 
 def subtraction(num1, num2)
   sub = num1 - num2
-  return sub 
+  return sub
 end
 
 def division(num1, num2)
+  div = num1 / num2
+  return div
 end
 
 def multiplication(num1, num2)
+  mull = num1 * num2
+  return mull
 end
 
 def modulo(num1, num2)
+  mod = num1 % num2
+  return mod
+
+  
 end
 
 def square_root(num)
